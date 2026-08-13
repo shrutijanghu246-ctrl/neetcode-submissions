88a56d6273bf0430dfacbde0847bea4421a38e1e
Shruti Janghu <shrutijanghu246@gmail.com>
2026-08-13 18:37:51 +0530

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 0 || x == 1){
+            return x;
+        }
+
+        int left = 1, right = x, ans = 0;
+        while(left <= right){
+            long long mid = left + (right - left)/2;
+            long long sq = mid * mid;
+
+            if(sq == x){
+                return mid;
+            }else if(sq < x){
+                ans = mid;
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
