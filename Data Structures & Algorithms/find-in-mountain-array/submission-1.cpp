b8c13e3b291b14a6356bbe33326b3bbb5e17ecd1
Shruti Janghu <shrutijanghu246@gmail.com>
2026-08-16 12:09:48 +0530

    Add: find-in-mountain-array - submission-1

---
+++ Data Structures & Algorithms/find-in-mountain-array/submission-1.cpp
@@ -0,0 +1,48 @@
+ /**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class MountainArray {
+ *   public:
+ *     int get(int index);
+ *     int length();
+ * };
+ */
+
+class Solution {
+public:
+    int findInMountainArray(int target, MountainArray &mountainArr) {
+        int n = mountainArr.length();
+
+        int l = 0, r = n - 1;
+        while(l < r){
+            int mid = l + (r - l)/2;
+            if(mountainArr.get(mid) < mountainArr.get(mid + 1)){
+                l = mid + 1; 
+            }else{
+                r = mid;
+            }
+        }
+
+        int peak = l;
+
+        l = 0, r = peak;
+        while(l <= r){
+            int mid = l + (r - l)/2;
+            int val = mountainArr.get(mid);
+            if (val == target) return mid;
+            else if (val < target) l = mid + 1;
+            else r = mid - 1;
+        }
+
+        l = peak + 1, r = n - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int val = mountainArr.get(mid);
+            if (val == target) return mid;
+            else if (val > target) l = mid + 1;
+            else r = mid - 1;
+        }
+
+        return -1;
+    }
+};
